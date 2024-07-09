1b2d725e102c863d7294a5e5fae930827acb8795
Dfsantana1 <dsantanafernandezmail.com>
2024-07-09 09:29:56 -0500

    Update TestLib.py

--- TestLib.py
+++ TestLib.py
@@ -1,10 +1,16 @@
 from robin_api import RobinAIClient
 
 # Inicializar clientes
-client = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client1 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
 client2 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client3 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client4 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client5 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client6 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client7 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
+client8 = RobinAIClient(api_key="Q9JccNBNBZqZEfGp7iEPt95E89RFJY")
 
-print('hi started the test by RobinAI API')
+print("hi started the test by RobinAI API")
 
 # Booleanos para seguimiento de éxito
 text_to_image_success = False
@@ -17,22 +23,22 @@ get_response_similar_sentences_success = False
 get_folder_files_success = False
 
 # 1. Generar imagen a partir de texto
-print('1. Generate to image')
+print("1. Generate to image")
 try:
-    image = client.completions.text_to_image(prompt="A beautiful sunset over the ocean with a pink and purple sky.")
-    print('\nThe URL is:', image.url)
+    image = client1.completions.text_to_image(prompt="A beautiful sunset over the ocean with a pink and purple sky.")
+    print("\nThe URL is:", image.url)
     text_to_image_success = True
 except AttributeError as e:
     print(f"Function text_to_image failed: {e}")
 
 # 2. Crear stream
-print('2. Create stream')
+print("2. Create stream")
 conversation = [
     {"role": "system", "content": "system_prompt"},
     {"role": "user", "content": "que es una llama ?"}
 ]
 try:
-    stream = client.completions.create_stream(
+    stream = client2.completions.create_stream(
         model="ROBIN_4",
         conversation=conversation,
         max_tokens=200,
@@ -49,9 +55,9 @@ except AttributeError as e:
     print(f"Function create_stream failed: {e}")
 
 # 3. Obtener respuesta similar a oraciones
-print('3. Get response similar sentences stream')
+print("3. Get response similar sentences stream")
 try:
-    get_answer = client2.completions.create(
+    get_answer = client3.completions.create(
         model="ROBIN_4",
         conversation=conversation,
         max_tokens=200,
@@ -64,9 +70,9 @@ except AttributeError as e:
     print(f"Function create_response failed: {e}")
 
 # 4. Crear carpeta y subir archivo
-print('4. Create Folder')
+print("4. Create Folder")
 try:
-    folder_information = client2.files.upload_file(url="https://arxiv.org/pdf/2302.13971.pdf")
+    folder_information = client4.files.upload_file(url="https://arxiv.org/pdf/2302.13971.pdf")
     apiFolderId = folder_information.folder.apiFolderId
     print(folder_information.folder.apiFolderId)
     create_folder_success = True
@@ -74,9 +80,9 @@ except AttributeError as e:
     print(f"Function create_folder failed: {e}")
 
 # 5. Obtener oraciones similares
-print('5. Get similar sentences')
+print("5. Get similar sentences")
 try:
-    files = client2.files.get_similar_sentences(
+    files = client5.files.get_similar_sentences(
         query="What are the practical implications of the findings in the document?",
         top=15,
         api_folder_id=apiFolderId,
@@ -85,7 +91,7 @@ try:
     print(files)
     get_similar_sentences_success = True
 
-    files_get = client2.files.get_response_similar_sentences_stream(
+    files_get = client6.files.get_response_similar_sentences_stream(
         model="ROBIN_4",
         max_new_tokens=200,
         top=1,
@@ -101,7 +107,7 @@ try:
             print(chunk.details, end="")
     get_response_similar_sentences_stream_success = True
 
-    file_test = client2.files.get_response_similar_sentences(
+    file_test = client7.files.get_response_similar_sentences(
         model="ROBIN_4",
         max_new_tokens=200,
         top=1,
@@ -113,7 +119,7 @@ try:
     print(file_test.message.choices[0].message.content)
     get_response_similar_sentences_success = True
 
-    get_folder_files = client2.files.get_folder_files(api_folder_id=apiFolderId)
+    get_folder_files = client8.files.get_folder_files(api_folder_id=apiFolderId)
     print(get_folder_files)
     get_folder_files_success = True
 except AttributeError as e:
